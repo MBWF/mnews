97fb60fd3ccd818233d1e6803f23b471e78eca24
marciofilho <marciofilho@fab.work>
2023-04-27 19:27:25 -0300

    chore: add dot

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
   return (
     <main className="flex flex-col items-center justify-between px-24 w-full">
       <h1 className="text-xl sm:text-2xl font-bold text-sky-950 my-8 whitespace-nowrap">
-        Update yourself about front-end world
+        Update yourself about front-end world.
       </h1>
       <div className="m-auto w-full">
         <Content posts={posts} />
